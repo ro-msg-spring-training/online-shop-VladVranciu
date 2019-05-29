041d7bdea19b60a35cd606e50c1692a9260dff09
VladVranciu <v.vranciu@yahoo.com>
2019-05-29 12:44:43 +0300

    chapter 4 completed

---
+++ shop/src/main/java/ro/msg/learning/shop/config/security/WebConfig.java
@@ -0,0 +1,25 @@
+//package ro.msg.learning.shop.config;
+//
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("PUT", "DELETE","GET", "POST","OPTIONS");
+////                .allowedOrigins("localhost:8080")
+////                .allowedMethods("PUT", "DELETE","GET","POST");
+////                .allowedHeaders("header1", "header2", "header3")
+////                .exposedHeaders("header1", "header2")
+////                .allowCredentials(false).maxAge(3600);
+//    }
+//}
